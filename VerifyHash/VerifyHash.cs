76840142d7e8ee2c629579578e0d72e227f5d377
ReenduX <krakers114pl@gmail.com>
2019-08-02 15:57:32 +0200

    VerifyHash v3

---
+++ VerifyHash/VerifyHash.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VerifyHash
+{
+    public class VerifyHash
+    {
+        string[][] scannedHashes;
+        public int filesToScan;
+        public int badFiles;
+        public int totalFilesScanned;
+        public int redownloadedCount;
+        public List<string> invalidFileList = new List<string>();
+
+        public VerifyHash()
+        {
+            string[] hashcontent = ExtractResource("VerifyHash.sha1_nfswfiles.txt").Split(new[] { '\n' });
+
+            scannedHashes = new string[hashcontent.Length][];
+            for (var i = 0; i < hashcontent.Length; i++)
+            {
+                scannedHashes[i] = hashcontent[i].Split(' ');
+            }
+
+            filesToScan = scannedHashes.Length;
+            badFiles = 0;
+            totalFilesScanned = 0;
+            redownloadedCount = 0;
+        }
+
+        public void ScanLines(Action<string> print)
+        {
+            Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories).AsParallel().ForAll((file) =>
+            {
+                for (var i = 0; i < scannedHashes.Length; i++)
+                {
+                    if (scannedHashes[i][1].Trim() == file.Replace(Directory.GetCurrentDirectory(), string.Empty).Trim())
+                    {
+                        if (scannedHashes[i][0].Trim() != Calculate(file).Trim())
+                        {
+                            badFiles++;
+                            print("[SCAN] Invalid hash \t | " + file.Replace(Directory.GetCurrentDirectory(), string.Empty));
+                            invalidFileList.Add(file.Replace(Directory.GetCurrentDirectory(), string.Empty).Trim());
+                        }
+                    }
+                }
+                totalFilesScanned++;
+            });
+        }
+
+        public void DownloadFiles(Action<string> print)
+        {
+            invalidFileList.AsParallel().ForAll((filename) => {
+                try
+                {
+                    string currentfile = Directory.GetCurrentDirectory() + filename;
+                    string downloadurl = "http://cdn.worldunited.gg/verify/unpacked" + filename.Replace("\\", "/");
+
+                    if (File.Exists(currentfile + ".vhbak")) File.Delete(currentfile + ".vhbak");
+                    File.Move(currentfile, currentfile + ".vhbak");
+
+                    WebClient downloader = new WebClient();
+                    downloader.DownloadFile(downloadurl, currentfile);
+
+                    redownloadedCount++;
+                }
+                catch
+                {
+                    print("[FILE] Status: Failed\t | " + filename);
+                }
+            });
+        }
+
+        #region helpers
+
+        private static string Calculate(string filename)
+        {
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] retVal = sha1.ComputeHash(File.OpenRead(filename));
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+
+            return sb.ToString().ToUpper();
+        }
+
+        private static string ExtractResource(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream(filename))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ VerifyHashStarter/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VerifyHashStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "VerifyFiles v0.3 Beta";
+            Console.WriteLine("VerifyFiles v0.3 Beta");
+            string copyrightText = "";
+            if (DateTime.Now.Year != 2019)
+            {
+                copyrightText = "2019-" + DateTime.Now.Year;
+            }
+            else
+            {
+                copyrightText = "2019";
+            }
+
+            Console.WriteLine("Copyright " + copyrightText + " - WorldUnited");
+            Console.WriteLine("Author: MeTonaTOR & ReenduX");
+            Console.WriteLine("------------------------------------------------------------");
+
+            if (!File.Exists("nfsw.exe"))
+            {
+                Console.WriteLine("Cannot find nfsw.exe, please place VerifyHash.exe directly to Need for Speed: World installation folder.");
+                Console.WriteLine("------------------------------------------------------------");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("This tool will verify your NFSW installation, if you agree \non that, press anything on keyboard.");
+            Console.WriteLine("Otherwise, shut down this app by clicking on X or ALT+F4.");
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------------------------");
+            bool taskDone = false;
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    if (taskDone != true)
+                    {
+                        Console.WriteLine("[INFO] Loading hashes");
+                        VerifyHash.VerifyHash vh = new VerifyHash.VerifyHash();
+                        TaskbarProgress.SetState(handle, TaskbarProgress.TaskbarStates.Indeterminate);
+
+                        Task.Run(() => {
+                            while (vh.totalFilesScanned != vh.filesToScan)
+                            {
+                                TaskbarProgress.SetValue(handle, (int)(100 * vh.totalFilesScanned / vh.filesToScan), 100);
+
+                                Console.Title = "[" + vh.totalFilesScanned + "/" + vh.filesToScan + "] VerifyFiles v0.2 Beta";
+
+                                Task.Delay(100);
+                            }
+                        });
+
+                        vh.ScanLines((output) => Console.WriteLine(output));
+
+                        if (vh.invalidFileList.Count != 0)
+                        {
+                            Console.WriteLine("------------------------------------------------------------");
+                            Console.Title = "VerifyFiles v0.2 Beta";
+                            Console.WriteLine("  Redownloading " + vh.invalidFileList.Count + " files: ");
+
+                            Task.Run(() => {
+                                while (vh.redownloadedCount != vh.badFiles)
+                                {
+                                    TaskbarProgress.SetValue(handle, (int)(100 * vh.redownloadedCount / vh.badFiles), 100);
+
+                                    Console.Title = "[" + vh.redownloadedCount + "/" + vh.badFiles + "] VerifyFiles v0.2 Beta";
+
+                                    Task.Delay(100);
+                                }
+                            });
+
+                            vh.DownloadFiles((output) => Console.WriteLine(output));
+                        }
+
+                        Console.WriteLine("------------------------------------------------------------");
+                        Console.WriteLine("  All done! Results: ");
+                        Console.WriteLine("    Total Files Scanned: " + vh.totalFilesScanned.ToString());
+                        Console.WriteLine("    Total Files To Scan: " + vh.filesToScan.ToString());
+                        Console.WriteLine("    Mismatched Hash: " + vh.badFiles.ToString());
+                        Console.WriteLine("    Redownloaded Files: " + vh.redownloadedCount.ToString());
+                        Console.WriteLine("------------------------------------------------------------");
+                        Console.WriteLine("Press ESC to quit.");
+                        taskDone = true;
+                    }
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
